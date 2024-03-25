46bbbb5a095a0132a72210a26339c43a4b4356ae
rafaelsideguide <150964962+rafaelsideguide@users.noreply.github.com>
2024-03-25 10:48:40 -0300

    initial commit.

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+import { MendableChatBubble } from "@mendable/search";
+
+export default function Footer() {
+  const style = { darkMode: false, accentColor: "#123456" }
+
+  return (
+    <footer>
+      <p>footer</p>
+
+      <MendableChatBubble
+        anon_key="f6fb7f0e-e7ac-4b08-a531-da0ee6cd2cc1"
+        style={style}
+      />
+    </footer>
+  );
+}
---
+++ pages/blog.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/footer";
+
+export default function About() {
+  return (
+    <div>
+      <h1>blog</h1>
+      
+      <Footer/>  
+    </div>
+  )
+}
